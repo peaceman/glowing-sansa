ab5c81cf607d46a5b72f3c89646995eff5852409
peaceman <nixalio@gmail.com>
2013-07-11 21:52:47 +0200

    generate devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LodgeManager::Application.routes.draw do
+  devise_for :admins
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
