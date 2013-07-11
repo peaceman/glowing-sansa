deba319856a8a379c9b207abcd6a806f7b1ebcbd
peaceman <nixalio@gmail.com>
2013-07-11 21:04:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LodgeManager::Application.config.secret_key_base = 'b0eb2651567828f3bcd3beb5da53b547273a3776e418cd77e6179ba6be0cbbbd567beb9c0bf02cb7b83cadc8ec5f6d79ee06de5ce712d281d7e5c1508c5e6e0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LodgeManager::Application.config.session_store :cookie_store, key: '_lodge-manager_session'
