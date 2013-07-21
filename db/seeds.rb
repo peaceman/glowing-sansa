b235563e0ba0f8cd19619a1419013f585efe3e55
peaceman <nixalio@gmail.com>
2013-07-21 16:00:30 +0200

    add grand_lodge abbreviation to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 grand_lodges = [
-    'Großloge der Alten Freien und Angenommenen Maurer von Deutschland (GL AFuAMvD)',
-    'Große Landesloge der Freimaurer von Deutschland (GLL FvD)',
-    'Große National-Mutterloge "Zu den drei Weltkugeln" (GNML 3WK)',
-    'American Canadian Grand Lodge A.F. & A.M (ACGL)',
-    'Grand Lodge of British Freemasons in Germany (GL BFG)',
+    {
+        name: 'Großloge der Alten Freien und Angenommenen Maurer von Deutschland',
+        abbreviation: 'GL AFuAMvD'
+    },
+    {
+        name: 'Große Landesloge der Freimaurer von Deutschland',
+        abbreviation: 'GLL FvD'
+    },
+    {
+        name: 'Große National-Mutterloge "Zu den drei Weltkugeln"',
+        abbreviation: 'GNML 3WK'
+    },
+    {
+        name: 'American Canadian Grand Lodge A.F. & A.M',
+        abbreviation: 'ACGL'
+    },
+    {
+        name: 'Grand Lodge of British Freemasons in Germany',
+        abbreviation: 'GL BFG'
+    }
 ]
 
 grand_lodges.each do |gl|
-  GrandLodge.find_or_create_by_name(gl)
+  grand_lodge = GrandLodge.find_or_initialize_by_name(gl[:name])
+  grand_lodge.update(
+      abbreviation: gl[:abbreviation]
+  )
 end
 
 User.create(email: 'foo@user.com', password: 'CrapLord', :is_admin => false)
